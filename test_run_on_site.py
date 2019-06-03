e98739703063b137d57c88b072df8be17c6a90f2
Nitsan Avni <nitsan@papayaglobal.com>
2019-06-03 21:13:03 +0300

    wip selenium

---
+++ test_run_on_site.py
@@ -0,0 +1,42 @@
+import pytest
+import time
+import os
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from io import BytesIO
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    yield driver
+    sleep(7)
+    driver.close()
+
+
+@pytest.fixture
+def wait(driver):
+    return WebDriverWait(driver, 10)
+
+
+def test_example(driver, wait):
+    driver.get('http://play.elevatorsaga.com/#challenge=8')
+    codeAreaSelector = "body > div > div.code > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div > div.CodeMirror-code > div:nth-child(1) > pre"
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, codeAreaSelector)))
+    codeAreaElement = driver.find_element_by_css_selector(codeAreaSelector)
+    code = open("packed.js", 'r').read()
+    ActionChains(driver) \
+        .click(codeAreaElement) \
+        .key_down(Keys.COMMAND) \
+        .send_keys("a") \
+        .key_up(Keys.COMMAND) \
+        .perform()
+    elem = driver.switch_to.active_element
+    elem.send_keys("hello" + str(elem.get_attribute("tagName")))
+    # driver.execute_script("document.getElementById('idName').setAttribute('value', 'text_to_put');
+    driver.save_screenshot("screenshot.png")
