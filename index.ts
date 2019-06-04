02175d343c1cf22936684cd3a01512175c0d0f47
Nitsan Avni <nitsan@papayaglobal.com>
2019-06-04 22:35:04 +0300

    better algo

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import each from "lodash/each";
 import some from "lodash/some";
+import minBy from "lodash/minBy";
 
 type FloorNumber = number;
 
 interface Elevator {
-    idle: boolean;
     goToFloor(floorNumber: FloorNumber): void;
-    on(event: "floor_button_pressed", cb: (param: FloorNumber) => void): void;
-    on(event: "idle", cb: () => void): void;
+    on(event: "floor_button_pressed" | "idle", cb: ((param: FloorNumber) => void) | (() => void)): void;
     currentFloor(): FloorNumber;
+    goingUpIndicator: (value: boolean | void) => void | boolean;
+    goingDownIndicator: (value: boolean | void) => void | boolean;
     destinationQueue: Array<FloorNumber>;
 }
 
@@ -19,27 +20,32 @@ interface Floor {
 
 type Seconds = number;
 
-export = {
-    init: (elevators: Elevator[], floors: Floor[]) => {
-        const n = (f: FloorNumber | Floor): number => (typeof f == "number" ? f : f.floorNum());
-        const queued = (e: Elevator, f: FloorNumber | Floor): boolean => e.destinationQueue.includes(n(f));
-        const queuedOnAnyElevator = (f: FloorNumber | Floor) => some(elevators, (e) => queued(e, f));
-        const go = (e: Elevator, f: FloorNumber | Floor) => {
-            if (!queued(e, f)) e.goToFloor(n(f));
-        };
-        const goIfFirst = (e: Elevator, f: FloorNumber | Floor) => {
-            if (!queuedOnAnyElevator(n(f))) go(e, f);
-        };
-        const up = (e: Elevator) => (e.currentFloor() + 1) % floors.length;
-        const goUp = (e: Elevator) => go(e, up(e));
-        const onCall = (f: Floor, cb: (f: FloorNumber) => void) => {
-            f.on("up_button_pressed", cb);
-            f.on("down_button_pressed", cb);
-        };
-        const allGo = (f: FloorNumber) => elevators.map((e) => goIfFirst(e, f));
+const init = (elevators: Elevator[], floors: Floor[]) => {
+    const n = (f: FloorNumber | Floor): number => (typeof f == "number" ? f : f.floorNum());
+    const queued = (e: Elevator, f: FloorNumber | Floor): boolean => e.destinationQueue.includes(n(f));
+    const queuedOnAnyElevator = (f: FloorNumber | Floor) => some(elevators, (e) => queued(e, f));
+    const go = (e: Elevator, f: FloorNumber | Floor) => {
+        if (!queued(e, f)) e.goToFloor(n(f));
+    };
+    const goIfFirst = (e: Elevator, f: FloorNumber | Floor) => {
+        if (!queuedOnAnyElevator(n(f))) go(e, f);
+    };
+    const up = (e: Elevator) => (e.currentFloor() + 1) % floors.length;
+    const goUp = (e: Elevator) => go(e, up(e));
+    const onCall = (f: Floor, cb: (f: FloorNumber) => void) => {
+        f.on("up_button_pressed", cb);
+        f.on("down_button_pressed", cb);
+    };
+    const allGo = (f: FloorNumber) => elevators.map((e) => goIfFirst(e, f));
 
-        each(floors, (f) => onCall(f, allGo));
-        each(elevators, (e) => e.on("floor_button_pressed", (n: FloorNumber) => go(e, n)));
-    },
+    const leastBusy: () => Elevator = () => minBy(elevators, (e) => e.destinationQueue.length) as Elevator;
+    const sendLeastBusyElevator = (n: FloorNumber) => go(leastBusy(), n);
+
+    each(floors, (f) => onCall(f, sendLeastBusyElevator));
+    each(elevators, (e) => e.on("floor_button_pressed", (n: FloorNumber) => go(e, n)));
+};
+
+export = {
+    init,
     update: (dt: Seconds, elevators: Elevator[], floors: Floor[]) => {}
 };
